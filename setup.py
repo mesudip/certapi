79bbd2de0e89858b3e56c2a987b3458dfb801f39
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2025-08-24 20:47:29 +0545

    Prepare release 0.4.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="certapi",
-    version="0.4.6",
+    version="0.4.8",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
